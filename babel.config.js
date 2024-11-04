2ce48143d17c2e44feb83fe0af26b823b5a24fee
Sakshi <85516345+krsakshi@users.noreply.github.com>
2024-11-04 19:06:41 +0530

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react', // Add this preset for React support
+      '@babel/preset-typescript', // Add this preset for TypeScript support
+    ],
+  };
+  
